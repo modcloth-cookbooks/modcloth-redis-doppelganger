851b8d28c64b123814f2ca0063be80d7a43f2288
Roberto Sanchez <sanchez.sanchez.roberto@gmail.com>
2013-04-15 11:31:07 -0500

    Removed unnecessary template for default recipe

--- recipes/default.rb
+++ recipes/default.rb
@@ -2,10 +2,6 @@ package node['redis']['pkg_name'] do
   action :install
 end
 
-template "#{node['redis']['dir']}/redis.conf" do
-  source 'redis.conf.erb'
-end
-
 service node['redis']['service_name'] do
   action [:enable, :start]
 end
