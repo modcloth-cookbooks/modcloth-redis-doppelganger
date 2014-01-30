92dbb398f9fff8aeea9f6f3a3da0e21d4d45df83
Roberto Sanchez <r.sanchez@modcloth.com>
2014-01-29 16:31:01 -0800

    Updated condition for slave redis vms (only needed for version 0.2.0 and up

--- recipes/slave_aof.rb
+++ recipes/slave_aof.rb
@@ -1,3 +1,5 @@
-node.set[:redis][:slaveof] = search("node", "roles:redis-sessions AND chef_environment:#{node.chef_environment}").first
+if node['application'] && node['application']['environment']
+  node.set[:redis][:slaveof] = search('node', "roles:redis-sessions AND application_environment:#{node['application']['environment']}").first
+end
 
 include_recipe 'redis::rails_sessions_aof'
